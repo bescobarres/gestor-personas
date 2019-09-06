4d4fca7266afc8ec48f0699e9d45d9ec998267fd
Bryan Escobar Restrepo <bryan.escobar@ceiba.com.co>
2019-09-06 09:48:43 -0500

    [GP]: Commit inicial

---
+++ src/main/java/co/com/tl/gestorpersonas/GestorPersonasApplication.java
@@ -0,0 +1,14 @@
+package co.com.tl.gestorpersonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorPersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorPersonasApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/co/com/tl/gestorpersonas/GestorPersonasApplicationTests.java
@@ -0,0 +1,16 @@
+package co.com.tl.gestorpersonas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestorPersonasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
